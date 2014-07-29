2750472cd9ae55263fd15ec11383840c36df5b0a
jacobyan <yanxugg@163.com>
2014-07-29 15:39:37 +0800

    update file header

--- examples/Slave_temperature/Slave_temperature.ino
+++ examples/Slave_temperature/Slave_temperature.ino
@@ -1,32 +1,35 @@
 /*
-BluetoothShield Demo Code Slave.pde. This sketch could be used with
-Master.pde to establish connection between two Arduino. It can also
-be used for one slave bluetooth connected by the device(PC/Smart Phone)
-with bluetooth function.
-2011 Copyright (c) Seeed Technology Inc.  All right reserved.
+ * FM.h
+ * A library for SeeedStudio Grove FM
+ *
+ * Copyright (c) 2012 seeed technology inc.
+ * Website    : www.seeed.cc
+ * Author     : Steve Chang
+ * Create Time: JULY 2014
+ * Change Log : Modified by loovee 2013-10-29  ,   Modified by jacob yan 2014-7-29  
+ 
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-Author: Steve Chang
 
-This demo code is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-For more details about the product please check http://www.seeedstudio.com/depot/
-
-*/
-
-
-/* Upload this sketch into Seeeduino and press reset*/
 
 #include <SoftwareSerial.h>   //Software Serial Port
 #define RxD 7
@@ -38,6 +41,13 @@ For more details about the product please check http://www.seeedstudio.com/depot
 
 
 SoftwareSerial blueToothSerial(RxD,TxD);
+
+/***************************************************************************
+ * Function Name: getTemp
+ * Description:  get the temperature data
+ * Parameters:  
+ * Return: 
+***************************************************************************/
 
 int getTemp()
 {
@@ -87,15 +97,18 @@ void loop()
 }
 
 
-
+/***************************************************************************
+ * Function Name: setupBlueToothConnection
+ * Description:  initilizing bluetooth connction
+ * Parameters: 
+ * Return: 
+***************************************************************************/
 void setupBlueToothConnection()
 {          
-    blueToothSerial.begin(9600);  
-
-	blueToothSerial.print("AT+BAUD6");                                                                           
-	delay(400); 
+	blueToothSerial.begin(9600);  
 	
-	blueToothSerial.begin(38400); 
+	blueToothSerial.print("AT");
+	delay(400); 
 
 	blueToothSerial.print("AT+NAMESeeedBTSlave");    // set the bluetooth name as "SeeedBTSlave" ,the length of bluetooth name must less than 12 characters.
 	delay(400);
